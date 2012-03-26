f5576ec6c3176446632506abcca143bef1d4c32c
Benjamin Roberts <animalrobot@gmail.com>
2012-03-25 21:15:36 -0400

    basic omniauth works!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   
+  def test
+  end
+  
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
